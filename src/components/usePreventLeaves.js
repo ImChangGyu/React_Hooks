3328464136fdd995e9b1722232b0997c44afc639
jadu20 <audu1217@naver.com>
2021-02-20 20:36:26 +0900

    [update]usePreventLeaves.js

---
+++ src/components/usePreventLeaves.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const usePreventLeave = () => {
+  const listener = (event) => {
+    event.preventDefault();
+    event.returnValue = "";
+  };
+  const enablePrevent = () => {
+    window.addEventListener("beforeunload", listener);
+  };
+  const disablePrevent = () => {
+    window.removeEventListener("beforeunload", listener);
+  };
+  return { enablePrevent, disablePrevent };
+};
+
+const usePreventLeaves = () => {
+  const { enablePrevent, disablePrevent } = usePreventLeave();
+  return (
+    <div>
+      <button onClick={enablePrevent}>Protect</button>
+      <button onClick={disablePrevent}>Unprotect</button>
+    </div>
+  );
+};
+
+export default usePreventLeaves;
