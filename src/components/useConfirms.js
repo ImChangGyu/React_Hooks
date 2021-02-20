4f2a2731e3c32d53b4cecec69659b87304bc7489
jadu20 <audu1217@naver.com>
2021-02-20 20:36:09 +0900

    [update]useConfirms.js

--- src/components/useConfirms.js
+++ src/components/useConfirms.js
@@ -1,7 +1,32 @@
 import React, { useState, useEffect, useRef } from "react";
 
+const useConfirm = (message = "", onConfirm, onCancle) => {
+  if (!onConfirm || typeof onConfirm !== "function") {
+    return;
+  }
+  if (!onCancle || typeof onCancle !== "function") {
+    return;
+  }
+  const confirmAction = () => {
+    // eslint-disable-next-line no-restricted-globals
+    if (confirm(message)) {
+      onConfirm();
+    } else {
+      onCancle();
+    }
+  };
+  return confirmAction;
+};
+
 const useConfirms = () => {
-  return;
+  const deleteWorld = () => console.log("Deleting the world....");
+  const abort = () => console.log("Aborted");
+  const confirmDelete = useConfirm("Are you sure", deleteWorld, abort);
+  return (
+    <div>
+      <button onClick={confirmDelete}>Delete the world</button>
+    </div>
+  );
 };
 
 export default useConfirms;
