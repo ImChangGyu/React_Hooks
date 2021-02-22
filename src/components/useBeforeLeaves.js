4d1eb4a6f201f45f07dae05a59ed20e795a8e9d0
jadu20 <audu1217@naver.com>
2021-02-22 16:18:39 +0900

    [add]useBeforeLeaves.js

---
+++ src/components/useBeforeLeaves.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+
+const useBeforeLeave = (onBefore) => {
+  useEffect(() => {
+    document.addEventListener("mouseleave", handle);
+    return () => document.removeEventListener("mouseleave", handle);
+  }, []);
+  if (typeof onBefore !== "function") {
+    return;
+  }
+  const handle = () => {
+    console.log("leaving");
+  };
+};
+
+const useBeforeLeaves = () => {
+  const begForLife = () => console.log("Please don't leave");
+  useBeforeLeave(begForLife);
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default useBeforeLeaves;
