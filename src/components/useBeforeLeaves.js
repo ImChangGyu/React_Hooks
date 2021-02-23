01ae303bc62cd232ea2c4dad926916e199f95d72
jadu20 <audu1217@naver.com>
2021-02-23 23:49:28 +0900

    [update]useBeforeLeaves.js

--- src/components/useBeforeLeaves.js
+++ src/components/useBeforeLeaves.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
-import ReactDOM from "react-dom";
+import React, { useEffect } from "react";
 
 const useBeforeLeave = (onBefore) => {
   useEffect(() => {
