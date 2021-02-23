08320b3c4d8acca234a360284ef6baa6496f55ce
jadu20 <audu1217@naver.com>
2021-02-23 23:49:08 +0900

    [update]useConfirms.js

--- src/components/useConfirms.js
+++ src/components/useConfirms.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 
 const useConfirm = (message = "", onConfirm, onCancle) => {
   if (!onConfirm || typeof onConfirm !== "function") {
